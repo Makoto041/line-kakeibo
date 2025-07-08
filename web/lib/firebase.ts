c3c04217ddff18b9e1702cdcb843122a8d5d179b
buchi <45158922+Makoto041@users.noreply.github.com>
2025-07-08 14:14:23 +0900

    firebase.ts を更新

--- web/lib/firebase.ts
+++ web/lib/firebase.ts
@@ -3,12 +3,11 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, signInAnonymously, UserCredential } from 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCC5zztgElGW-ORnXMKA9LeqH0XilcU39c",
   authDomain: "line-kakeibo-0410.firebaseapp.com",
   projectId: "line-kakeibo-0410",
   storageBucket: "line-kakeibo-0410.appspot.com",
   messagingSenderId: "440748785600",
-  appId: "1:440748785600:web:2319a5fe3e7a7d2571d225",
+  appId: "",
   measurementId: "G-PLNC7GY160",
 };
 
